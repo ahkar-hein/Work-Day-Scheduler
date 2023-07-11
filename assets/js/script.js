f7d278ad1ef0efe1ac444fe59137d8fd75a8c5d6
Ahkar Hein <ahkarhein04@gmail.com>
2023-07-10 23:00:06 -0700

    applied style for past, present and future in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,4 +34,16 @@ $(function () {
     var timeBlockId = $(this).parent().attr('id'); 
     localStorage.setItem(timeBlockId, description);
   });
+
+  var currentHour = dayjs().format('H'); 
+  $('.time-block').each(function () {
+    var timeBlockHour = parseInt($(this).attr('id').split('-')[1]);
+    if (timeBlockHour < currentHour) {
+      $(this).removeClass('present future').addClass('past'); 
+    } else if (timeBlockHour == currentHour) {
+      $(this).removeClass('past future').addClass('present');
+    } else if (timeBlockHour > currentHour) {
+      $(this).removeClass('past present').addClass('future'); 
+    }
+  });
 });

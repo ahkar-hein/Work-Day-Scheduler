e28fa72d429dfd47414a50c11d69c99b98e91afd
Ahkar Hein <ahkarhein04@gmail.com>
2023-07-11 16:00:48 -0700

    added comment in js and html file and updated readme file git add .

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Work-Day-Scheduler
+## Description
+   Work Day Scheduler is a simple calendar application designed to help you schedule your work day effectively. With this application, you can easily plan and organize your daily tasks and events, ensuring maximum productivity.      
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Mock-Up](#mock-up)
+* [Screenshot](#screenshot)
+* [License](#license)
+
+## Installation 
+    Simply click the link that provided in the usage and the website should be run on your system default browser.
+
+## Usage
+### Website Link
+* []
+
+## Mock-Up
+
+![Alt text](/assets/images/2.gif)
+
+## Screentshot
+
+![Alt text](/assets/images/1.png)
+
+## License 
+    N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,38 +6,51 @@
 // save localstorage
 // retrieve from localstorage
 $(function () {
+  // Getting the current date by using dayjs
   var currentDate = dayjs().format('dddd, MMMM D, YYYY'); 
+  // display the date in header.
   $('#currentDay').text(currentDate);
+  // event listener click  for save button
   $('.saveBtn').on('click', function () {
-    var description = $(this).siblings('.description').val().trim(); 
+    // Getting user input from textarea
+    var description = $(this).siblings('.description').val().trim();
+    // getting id of the parent time block.
     var timeBlockId = $(this).parent().attr('id'); 
+    // save id and description into localstorage 
     localStorage.setItem(timeBlockId, description);
   });
 
+  // declare the variable to get current date
   var storedDate = localStorage.getItem('currentDate');
-  console.log(storedDate);
+  // if condition for current date not equal to storeDate
   if (currentDate !== storedDate) {
-    localStorage.clear(); 
-    localStorage.setItem('currentDate', currentDate);
+    localStorage.clear(); // Clear the localstorage.
+    localStorage.setItem('currentDate', currentDate); // set the new currentDate after clear.
   }
 
+  // Declare variable for currentHour by using dayjs 
   var currentHour = dayjs().format('H'); 
   $('.time-block').each(function () {
+    // getting the exact hour from time block id 
     var timeBlockHour = parseInt($(this).attr('id').split('-')[1]);
     if (timeBlockHour < currentHour) {
+      // set the color if hours is last then current
       $(this).removeClass('present future').addClass('past'); 
     } else if (timeBlockHour == currentHour) {
+      // set the color if hours is same as current
       $(this).removeClass('past future').addClass('present');
     } else if (timeBlockHour > currentHour) {
+      // set the color if the hours is greater than current
       $(this).removeClass('past present').addClass('future'); 
     }
   });
 
+  // retrive save data and display into textarea.
   $('.time-block').each(function () {
     var timeBlockId = $(this).attr('id');
     var savedDescription = localStorage.getItem(timeBlockId);
     if (savedDescription) {
-      $(this).find('.description').val(savedDescription);
+      $(this).find('.description').val(savedDescription); //set the values of the textarea from the save data from localstorage.
     }
   });
 });
